9a13d967d287798c7c926eff0ccb856318276177
rachin <rachin@pepo.com>
2017-10-24 03:12:13 +0530

    use 'combined' morgan log format

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
