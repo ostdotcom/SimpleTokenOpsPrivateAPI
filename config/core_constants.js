123e3a9919985c8e877533c2d3b4dfb8d56f465b
rachin <rachin@pepo.com>
2017-10-24 13:34:07 +0530

    jwt_auth

--- config/core_constants.js
+++ config/core_constants.js
@@ -20,3 +20,4 @@ define("PI", 3.14);
 
 define("ST_PUBLIC_GETH_BASE_URL", process.env.ST_PUBLIC_GETH_BASE_URL);
 define("ST_PRIVATE_GETH_PROVIDER", process.env.ST_PRIVATE_GETH_PROVIDER);
+define("ST_JWT_PRIVATE_KEY", "somethingsarebetterkeptinenvironemntvariables");
--- routes/token_sale.js
+++ routes/token_sale.js
@@ -6,6 +6,7 @@
  * * Date: 23/10/2017
  * * Reviewed by: Sunil
  */
+
 var express = require('express')
   , router = express.Router()
   , web3Validator = require('../lib/web3/validator')
@@ -15,7 +16,10 @@ var express = require('express')
   , whitelistContractAddress = coreAddresses.getAddressForContract('whitelister')
   , tokenSaleContractAbi = require('../abi/TokenSale')
   , myContract = new web3Provider.eth.Contract(tokenSaleContractAbi)
-  , publicEthereum = require("../lib/request/public_ethereum");
+  , publicEthereum = require("../lib/request/public_ethereum")
+  , jwtAuth = require("../lib/jwt/jwt_auth")
+;
+
 
 /* GET users listing. */
 router.post('/whitelist', function(req, res, next) {
@@ -43,5 +47,70 @@ router.post('/whitelist', function(req, res, next) {
   }
   
 });
+
+
+router.get('/test-valid', function(req, res, next) {
+	var data = {
+		"signed_tx" : "ksdjfgjksfksdjfkasjhdfkasjdfh"
+	};
+	var token = jwtAuth.issueToken( data );
+
+	jwtAuth.verifyToken( token, function(err, decodedToken) {
+		if (err) {
+			return res.json(200, {
+				success: false, 
+				data: {
+					"token": token
+				},
+				error: {
+					"msg": 'The token is not valid'
+				}
+			});
+		}
+		return res.json(200, {
+			success: true, 
+			data: {
+				"token": token,
+				"decodedToken" : decodedToken
+			}
+		});
+	}); 
+
+	res.send("token: |" + token + "|");
+});
+
+
+router.get('/test-invalid', function(req, res, next) {
+	var data = {
+		"signed_tx" : "ksdjfgjksfksdjfkasjhdfkasjdfh"
+	};
+	var token = jwtAuth.issueToken( data );
+
+	var malformedToken = token + "asdhaksdgjahgsdjhagsd";
+	jwtAuth.verifyToken( malformedToken, function(err, decodedToken) {
+		if (err) {
+			return res.json(200, {
+				success: false, 
+				data: {
+					"token": token,
+					"malformedToken": malformedToken
+				},
+				error: {
+					"msg": 'The token is not valid'
+				}
+			});
+		}
+		return res.json(200, {
+			success: true, 
+			data: {
+				"token": token,
+				"decodedToken" : decodedToken
+			}
+		});
+	}); 
+
+	res.send("token: |" + token + "|");
+});
+
 
 module.exports = router;
