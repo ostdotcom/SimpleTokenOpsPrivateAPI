d06bf2d81dcbda720b232343fd68cdf9c1227b77
abhay-pepo <abhay@pepo.com>
2017-10-27 19:32:56 +0530

    Echo All Env Variables

---
+++ config/echo_env_variables.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+ * Addresses
+ *
+ * * Author: Abhay
+ * * Date: 27/10/2017
+ * * Reviewed by:
+ *
+ * node config/echo_env_variables.js
+ */
+
+console.log("===Starting Printing All Environment Variables===\n");
+console.log(process.env);
+console.log("\n===Completed Printing All Environment Variables===");
