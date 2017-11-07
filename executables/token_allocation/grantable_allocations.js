25eb63ac96fd3a73826aba1825b81f693e294b5a
abhay-pepo <abhay@pepo.com>
2017-11-07 06:44:14 +0530

    grant Allocation Script

---
+++ executables/token_allocation/grantable_allocations.js
@@ -0,0 +1,116 @@
+"use strict";
+/*
+ * Grantable Allocations
+ *
+ * * Author: Abhay
+ * * Date: 04/11/2017
+ * * Reviewed by: Sunil
+ */
+
+// STEPS
+//
+// READ CSV
+// VALIDATE data
+// TO checksum
+// Prompt for confirmation for feeding
+// Call contract addGrantableAllocation method
+// Call Lock method
+// Bignumber handling
+
+const coreAddresses = require('../../config/core_addresses')
+  , readline = require('readline')
+  , bigNumber = require('bignumber.js')
+  , web3RpcProvider = require('../../lib/web3/rpc_provider')
+  , helper = require('./helper');
+
+const grantableAllocations = {
+
+  validateAndParse: function (data, contractAddresses, maxEntriesPerContract) {
+
+    return new Promise(function (onResolve, onReject) {
+
+      var totalEntries = data.length;
+
+      if (totalEntries == 0) {
+        console.error("No data present in csv!");
+        process.exit(1);
+      }
+
+      const maxContractsNeeded = Math.ceil(totalEntries / maxEntriesPerContract);
+
+      if (contractAddresses.length != maxContractsNeeded) {
+        console.error("Contract addresses count: " + contractAddresses.length + " does not match with maxContractsNeeded: " + maxContractsNeeded);
+        process.exit(1);
+      }
+
+      var parsedData = [];
+      for (var i = 0; i < totalEntries; i++) {
+
+        var grantData = data[i],
+          receiverAddr = grantData[0].trim(),
+          amount = new bigNumber(grantData[1].trim()),
+          isRevokable = grantData[2].trim().toLowerCase();
+
+        if (!web3RpcProvider.utils.isAddress(receiverAddr)) {
+          console.error("Receiver Address " + receiverAddr + " is not a valid ethereum address");
+          process.exit(1);
+        }
+
+        if (amount.isZero()) {
+          console.error("Amount 0 is not allowed for receiver " + receiverAddr);
+          process.exit(1);
+        }
+
+        if (!(["true", "false"].includes(isRevokable))) {
+          console.error("Incorrect isRevokable: " + isRevokable + " value." + " Only true/false value allowed for isRevokable");
+          process.exit(1);
+        }
+
+        var isRevokableBool = (isRevokable == "true");
+        parsedData.push([web3RpcProvider.utils.toChecksumAddress(receiverAddr), amount, isRevokableBool]);
+      }
+
+      onResolve(parsedData);
+    });
+  },
+
+  performGrant: async function() {
+
+    const filePath = "../../data/grant_allocations.csv",
+      contractName = 'grantableAllocations',
+      contractAddresses = coreAddresses.getAddressesForContract(contractName),
+      maxEntriesPerContract = 35,
+      senderName = 'postInitOwner';
+
+    var csvData = await helper.readCsv(filePath);
+
+    var parsedCsvData = await grantableAllocations.validateAndParse(csvData, contractAddresses, maxEntriesPerContract);
+
+    console.log("Max Entries " + maxEntriesPerContract + " will be entered per Contract");
+    console.log("Total Entries to process: " + csvData.length);
+    console.log("All Contract Addresses: " + contractAddresses);
+
+    var prompts = readline.createInterface(process.stdin, process.stdout);
+
+    await new Promise(
+      function (onResolve, onReject) {
+        prompts.question("Do you want to really do this? [Y/N]",
+          function (intent) {
+            if (intent === 'Y') {
+              console.log("Initiating GrantableAllocation script for contract: " + contractName);
+              prompts.close();
+              onResolve();
+            } else {
+              console.error('Exiting script.');
+              process.exit(1);
+            }
+          }
+        );
+      }
+    );
+    await helper.processFeedingData(contractName, senderName, parsedCsvData, maxEntriesPerContract, contractAddresses);
+  }
+
+};
+
+grantableAllocations.performGrant();
---
+++ executables/token_allocation/helper.js
@@ -0,0 +1,165 @@
+"use strict";
+/*
+ * Helper
+ *
+ * * Author: Abhay
+ * * Date: 04/11/2017
+ * * Reviewed by: Sunil
+ */
+
+const fs = require('fs')
+  , parseCsv = require('csv-parse')
+  , coreAddresses = require('../../config/core_addresses')
+  , getRawTx = require('../../lib/web3/get_raw_tx')
+  , web3Signer = require('../../lib/web3/signer')
+  , publicEthereum = require('../../lib/request/public_ethereum');
+
+const helper = {
+  readCsv: function (filePath) {
+
+    return new Promise(function (onResolve, onReject) {
+
+      var csvData = [];
+      fs.createReadStream(filePath)
+        .pipe(parseCsv({delimiter: ','}))
+        .on('data', function (csvrow) {
+          console.debug(csvrow);
+          //Push Row(Array) to csvData
+          csvData.push(csvrow);
+        })
+        .on('end', function () {
+          onResolve(csvData);
+        })
+        .on('error', function (e) {
+          console.error("Error: " + e + " in CSV parsing ");
+          process.exit(1);
+        });
+    });
+
+  },
+
+  // Send transaction for different calls
+  sendTransaction: function (txType, contractName, contractAddress, senderName, dataForTx) {
+
+    var rawTx = helper.getRawTransactionFor(txType, contractName, contractAddress, senderName, dataForTx);
+
+    // handle final response
+    const handlePublicOpsOkResponse = function (publicOpsResp) {
+      return new Promise(function (onResolve, onReject) {
+        const success = publicOpsResp.success;
+
+        if (success) {
+          console.log('----- Tx Successful');
+          console.log(publicOpsResp);
+          onResolve(publicOpsResp.data);
+        } else {
+          console.error('----- Tx Error');
+          console.error(publicOpsResp);
+          process.exit(1);
+        }
+
+      });
+    };
+
+    // Sign the transaction, send it to public ops machine, send response
+    return web3Signer.signTransactionBy(rawTx, senderName)
+      .then(publicEthereum.sendSignedTransaction)
+      .then(handlePublicOpsOkResponse);
+
+  },
+
+  // Poll for transaction receipt
+  verifyPublicOpsResponse: function(publicOpsRespData){
+    return new Promise(function(onResolve, onReject){
+      var txSetInterval = null;
+
+      var handleGetTxInfoResponse = function (response) {
+        console.log("getTransactionInfo API response: ");
+        console.log(response);
+        if (response.success) {
+          clearInterval(txSetInterval);
+          onResolve(response);
+        } else {
+          console.log('Waiting for ' + publicOpsRespData.transaction_hash + ' to be mined');
+        }
+      };
+
+      txSetInterval = setInterval(
+        function(){
+          publicEthereum.getTransactionInfo(publicOpsRespData.transaction_hash).then(handleGetTxInfoResponse);
+        },
+        5000
+      );
+
+    });
+  },
+
+  // feed data in contract with Max 35 logic
+  processFeedingData: function (contractName, senderName, data, maxEntriesPerContract, contractAddresses) {
+    return new Promise(async function (onResolve, onReject) {
+
+      var contractAddress = null
+        , contractAddressIndex = 0;
+
+      var callLockContract = function (contractAddress) {
+        return new Promise(async function (onResolve, onReject) {
+          console.log("\nInitiating locking of contract address: " + contractAddress + " for contract name: " + contractName + " for sender name: " + senderName);
+          await helper.sendTransaction('callLockContract', contractName, contractAddress, senderName, [])
+            .then(helper.verifyPublicOpsResponse)
+            .then(function(data){console.log(data.data.events_data[0].events);});
+          onResolve();
+        });
+      }
+
+      for (var i = 0; i < data.length; i++) {
+
+        contractAddress = contractAddresses[contractAddressIndex];
+        console.log("Iteration: " + i + " at address: " + contractAddress + " Data to Process: " + data[i]);
+        // Call add data method of contract
+        await helper.sendTransaction('callAddData', contractName, contractAddress, senderName, data[i])
+          .then(helper.verifyPublicOpsResponse)
+          .then(function(data){console.log(data.data.events_data[0].events);});
+        // If contract instance data entry is complete
+        if ( (((i + 1) % maxEntriesPerContract) === 0) || ((i + 1) == data.length) ) {
+          // Call lock method of contracts
+          await callLockContract(contractAddress);
+          contractAddressIndex += 1;
+          console.log("\n\n");
+        }
+
+      }
+      onResolve();
+    });
+
+  },
+
+  // NOTE:
+  //
+  // Below is code that need to be modified for different contracts
+  //
+
+  // Get raw transaction for transaction type
+  getRawTransactionFor: function(txType, contractName, contractAddress, senderName, dataForTx) {
+    if (txType == 'callLockContract') {
+      // define different lock methods for different contracts
+      return getRawTx.forLockContract(contractName, contractAddress, senderName);
+    } else if (txType == 'callAddData') {
+      // define different add data methods for different contracts
+      if ('grantableAllocations' == coreAddresses.getContractNameFor(contractAddress)) {
+        return helper.getTxForAddGrantableAllocation(contractName, contractAddress, senderName, dataForTx);
+      }
+    }
+    throw "Unhandled txType: " + txType + " for contract name: " + contractName + " at address: " + contractAddress;
+  },
+
+  // get raw transaction for addGrantableAllocation method
+  getTxForAddGrantableAllocation: function (contractName, contractAddress, senderName, dataForTx) {
+    var receiverAddr = dataForTx[0],
+      amount = dataForTx[1],
+      isRevokable = dataForTx[2];
+    return getRawTx.addGrantableAllocation(contractName, contractAddress, senderName, receiverAddr, amount, isRevokable);
+  }
+
+};
+
+module.exports = helper;
