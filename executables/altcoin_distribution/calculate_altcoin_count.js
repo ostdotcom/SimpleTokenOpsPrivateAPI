b0a32399e663f82e840b8c2f4da9b84cdbcc7665
aman_barbaria <aman@pepo.com>
2017-12-11 19:34:25 +0530

    calculate altcoin bonus count of tokens in wei

---
+++ executables/altcoin_distribution/calculate_altcoin_count.js
@@ -0,0 +1,104 @@
+"use strict";
+/*
+ * Altcoin Bonus Conversion Calculation
+ *
+ * * Author: Aman
+ * * Date: 11/12/2017
+ * * Reviewed by:
+ *
+ * node calculate_altcoin_count.js
+ *
+ */
+
+const readline = require('readline')
+    , bigNumber = require('bignumber.js')
+    , helper = require('../token_allocation/helper')
+    , fs = require('fs');
+
+const altcoinCalculation = {
+
+    calculateBonus: function (data, tokenDetails) {
+
+        return new Promise(function (onResolve, onReject) {
+
+            var totalEntries = data.length;
+            var parsedData = [];
+            for (var i = 0; i < totalEntries; i++) {
+
+                // Continue if blank value
+                if (!data[i] || data[i] == '') {
+                    continue;
+                }
+
+                var bonusData = data[i],
+                    receiverAddr = bonusData[0].trim(),
+                    tokenName = bonusData[1].trim(),
+                    ethAmountInWei = new bigNumber(bonusData[2].trim());
+
+                var tokenData = tokenDetails[tokenName];
+
+                var altcoinTokenInWei = ethAmountInWei.times(tokenData.ethToTokenRatio).floor().toNumber();
+
+                parsedData.push([receiverAddr, tokenName, ethAmountInWei, altcoinTokenInWei, tokenData.tokenContractAddress]);
+            }
+
+            onResolve(parsedData);
+        });
+    },
+
+    calculateTokenConversionRation: function (data) {
+
+        return new Promise(function (onResolve, onReject) {
+
+            var totalEntries = data.length;
+            var tokenDetails = {};
+
+            for (var i = 0; i < totalEntries; i++) {
+
+                var topkenData = data[i],
+                    tokenName = topkenData[0].trim(),
+                    tokenContractAddress = topkenData[1].trim(),
+                    totalEthSentInWei = new bigNumber(topkenData[2].trim()),
+                    totalTokensReceivedInWei = new bigNumber(topkenData[3].trim());
+
+                var ethToTokenRatio = totalTokensReceivedInWei.dividedBy(totalEthSentInWei);
+
+                var tokenDetail = {tokenContractAddress: tokenContractAddress, ethToTokenRatio: ethToTokenRatio};
+                tokenDetails[tokenName] = tokenDetail;
+
+            }
+
+            onResolve(tokenDetails);
+        });
+    },
+
+    writeToCsv: async function(csvData) {
+        var stream = fs.createWriteStream("altCoinBonusResults.csv");
+        stream.once('open', function (fd) {
+            csvData.forEach(function (csvData) {
+                stream.write(csvData[0] + "," + csvData[1] + "," + csvData[2] + "," + csvData[3] + "," + csvData[4] + "\n");
+            });
+            stream.end();
+        });
+    },
+
+    perform: async function() {
+
+        const altcoinPurchasefilePath = "../../data/altcoin_purchase_data.csv"
+        var altcoinPurchaseCsvData = await helper.readCsv(altcoinPurchasefilePath);
+
+        var tokenDetails = await altcoinCalculation.calculateTokenConversionRation(altcoinPurchaseCsvData);
+
+        const altcoinBonusfilePath = "../../data/altcoin_allocations.csv"
+        var altcoinBonusCsvData = await helper.readCsv(altcoinBonusfilePath);
+
+        var altcoinBonusOutputData = await altcoinCalculation.calculateBonus(altcoinBonusCsvData, tokenDetails);
+
+        console.log('Writing to Csv total count:' + altcoinBonusOutputData.length);
+
+        await altcoinCalculation.writeToCsv(altcoinBonusOutputData);
+    }
+
+};
+
+altcoinCalculation.perform();
