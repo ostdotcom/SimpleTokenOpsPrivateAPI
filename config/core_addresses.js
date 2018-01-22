9bf1569e60d4783f1e3c57135ac230c3926a8cf4
aman_barbaria <aman@pepo.com>
2018-01-22 19:30:48 +0530

    fix for whitelist env variable read

--- config/core_addresses.js
+++ config/core_addresses.js
@@ -13,7 +13,7 @@ const allAddresses = {
 
   users: {
     whitelister: {
-      address: process.env.ST_WHITELIST_ACCOUNT_ADDRS,
+      address: JSON.parse(process.env.ST_WHITELIST_ACCOUNT_ADDRS),
       passphrase: process.env.ST_WHITELIST_ACCOUNT_PASSPHRASE
     },
     postInitOwner: {
