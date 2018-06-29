c1583085ba325056d6aad7216ae1da02f30ac782
Puneet Khushwani <puneet.khushwani@ost.com>
2018-06-29 18:50:23 +0530

    bug fix for grantable / processable distributions

--- executables/token_allocation/helper.js
+++ executables/token_allocation/helper.js
@@ -78,7 +78,8 @@ const helper = {
       var handleGetTxInfoResponse = function (response) {
         console.log("getTransactionInfo API response: ");
         console.log(response);
-        if (response.success) {
+        var status = (response['data'] || {})['status'];
+        if (response.success && (status === 'mined' || status === 'failed')) {
           clearInterval(txSetInterval);
           onResolve(response);
         } else {
